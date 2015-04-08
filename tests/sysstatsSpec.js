9d1bef270f342558bdab95ed49a93a4e5966461e
Jamy Timmermans <me@jamy.be>
2015-04-08 20:32:29 +0200

    Fixed a bug thanks to tests

--- tests/sysstatsSpec.js
+++ tests/sysstatsSpec.js
@@ -1,10 +1,17 @@
 require("jasmine-beforeall");
 
 var stats = require("../lib/sysstats.js")();
+var os = require("os");
 
 describe("Sysstats", function () {
     it("should be a function", function () {
         expect(require("../lib/sysstats.js") instanceof Function).toBe(true);
+    });
+
+    it("should return the same listener when called again", function () {
+        var stats1 = require("../lib/sysstats.js")();
+        var stats2 = require("../lib/sysstats.js")();
+        expect(stats1).toBe(stats2);
     });
 
     it("should have a .cpus method which returns the cpus", function () {
@@ -26,12 +33,21 @@ describe("Sysstats", function () {
         }, 1000);
     });
 
-    it("should create a new listener after unref and recall", function () {
+    it("should create a new listener after unref and recall", function (next) {
         var oldListener = stats;
         stats.unref();
         stats = require("../lib/sysstats.js")();
 
         expect(stats).toNotBe(oldListener);
+        next();
+    });
+
+
+    it("should return the cpu info", function () {
+        var cpuDet = stats.getCPUInfo();
+        for (var i = 0; i < cpuDet.length; ++i) {
+            expect(cpuDet[i]).toEqual(os.cpus()[i].model);
+        }
     });
 
     it("should have a mem method which returns the used and total memory", function () {
